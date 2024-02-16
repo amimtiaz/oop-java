8c7be8ee7f174f07dac17cf32d110bc6b8fb198f
amimtiaz <amemtiaj19@gmail.com>
2024-02-16 10:52:59 +0600

    first commit

---
+++ app/src/main/java/com/imtiaz_acedamy/oop_class/MainActivity.java
@@ -0,0 +1,14 @@
+package com.imtiaz_acedamy.oop_class;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
